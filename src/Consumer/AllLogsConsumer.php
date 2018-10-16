d9f9173349f8777383bb66a077798b0a25b68e55
Marcin Malessa <marcin.malessa@performance-media.pl>
2018-10-16 14:36:22 +0200

    Consumer - small fixes

--- src/Consumer/AllLogsConsumer.php
+++ src/Consumer/AllLogsConsumer.php
@@ -22,21 +22,11 @@ class AllLogsConsumer implements ConsumerInterface
         return true;
         return false;
 
-        return ConsumerInterface::MSG_SINGLE_NACK_REQUEUE;  // 2
-        return ConsumerInterface::MSG_ACK;                  // 1
-        return ConsumerInterface::MSG_REJECT_REQUEUE;       // 0
-        return ConsumerInterface::MSG_REJECT;               // -1
-        return ConsumerInterface::MSG_ACK_SENT;             // -2
-
-/*
-        array(null, 'basic_ack'), // Remove message from queue only if callback return not false
-        array(true, 'basic_ack'), // Remove message from queue only if callback return not false
-        array(false, 'basic_reject', true), // Reject and requeue message to RabbitMQ
-        array(ConsumerInterface::MSG_ACK, 'basic_ack'), // Remove message from queue only if callback return not false
-        array(ConsumerInterface::MSG_REJECT_REQUEUE, 'basic_reject', true), // Reject and requeue message to RabbitMQ
-        array(ConsumerInterface::MSG_REJECT, 'basic_reject', false), // Reject and drop
-        array(ConsumerInterface::MSG_ACK_SENT), // ack not sent by the consumer but should be sent by the implementer of ConsumerInterface
-*/
-
+        return ConsumerInterface::MSG_SINGLE_NACK_REQUEUE;  // (??)
+        return ConsumerInterface::MSG_ACK;                  // Remove message from queue only if callback return not false
+        return ConsumerInterface::MSG_REJECT_REQUEUE;       // Reject and requeue message to RabbitMQ
+        return ConsumerInterface::MSG_REJECT;               // Reject and drop
+        return ConsumerInterface::MSG_ACK_SENT;             // ack not sent by the consumer but should be sent by the implementer of ConsumerInterface (??)
+        
     }
 }
